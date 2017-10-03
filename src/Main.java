2b8517bebbced7608f159c5721e14907bd427ba7
SeptiD <septid96@yahoo.com>
2017-10-03 19:27:59 +0300

    changed main class

--- src/Main.java
+++ src/Main.java
@@ -9,33 +9,33 @@ public class Main {
 		final GpioController gpio = GpioFactory.getInstance();
 		Car myCar = Car.getCar(gpio);
 		double distance;
-		for(int i=0;i<=1;i++){
-		boolean checker = true;
-		while(checker == true){
-			System.out.println("Going forwards");
-			myCar.moveForward();
-			
-			Thread.sleep(100);
-			
-			if((distance=myCar.getActualFrontDistance())<12.5 && myCar.getLastFrontDistance()<12.5){
-				checker = false;
+		for(int i=0;i<=1;i++) {
+			boolean checker = true;
+			while (checker == true) {
+				System.out.println("Going forwards");
+				myCar.moveForward();
+
+				Thread.sleep(100);
+
+				if ((distance = myCar.getActualFrontDistance()) < 12.5 && myCar.getLastFrontDistance() < 12.5) {
+					checker = false;
+				}
+				System.out.println("Distance" + distance);
+
+				System.out.println("Stop car");
+				myCar.stopCar();
+
+				Thread.sleep(3000);
+
+				System.out.println("Turn right");
+				myCar.turnRight();
+				Thread.sleep(5000);
+
+				myCar.stopCar();
+
+				Thread.sleep(3000);
 			}
-			System.out.println("Distance"+distance);
 		}
-		System.out.println("Stop car");
-		myCar.stopCar();
-		
-		Thread.sleep(1000);
-		
-		System.out.println("Turn right");
-		myCar.turnRight();
-		Thread.sleep(1000);
-		
-		myCar.stopCar();
-		
-		Thread.sleep(1000);
-		}
-		
 	
 		gpio.shutdown();
 		
